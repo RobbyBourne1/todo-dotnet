497b8d7fd5a987531178c1159da3a29a26cab736
Robby Bourne <robbybourne1@gmail.com>
2017-08-28 11:31:10 -0400

    Migrations script

---
+++ Migrations/20170828153000_addTodo.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace tododotnet.Migrations
+{
+    public partial class addTodo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
